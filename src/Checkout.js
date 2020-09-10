3b0c871857f837506b18f8157edfff8511cf95fe
Muhammad Irfan <36103202+mirfan46@users.noreply.github.com>
2020-09-11 00:31:16 +0700

    add animations

--- src/Checkout.js
+++ src/Checkout.js
@@ -3,6 +3,7 @@ import "./Checkout.css";
 import Subtotal from "./Subtotal";
 import { useStateValue } from "./StateProvider";
 import CheckoutProduct from "./CheckoutProduct";
+import FlipMove from "react-flip-move";
 
 function Checkout() {
   const [{ basket, user }, dispatch] = useStateValue();
@@ -19,16 +20,17 @@ function Checkout() {
         <div>
           <h3>Hello, {user?.email}</h3>
           <h2 className="checkout_title">Your shopping Basket</h2>
-
-          {basket.map((item) => (
-            <CheckoutProduct
-              id={item.id}
-              title={item.title}
-              image={item.image}
-              price={item.price}
-              rating={item.rating}
-            />
-          ))}
+          <FlipMove>
+            {basket.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </FlipMove>
         </div>
       </div>
 
--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,37 +1,39 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import "./CheckoutProduct.css";
 import { useStateValue } from "./StateProvider";
 
-function CheckoutProduct({ id, title, image, price, rating }) {
-  const [{ basket }, dispatch] = useStateValue();
+const CheckoutProduct = forwardRef(
+  ({ id, title, image, price, rating }, ref) => {
+    const [{ basket }, dispatch] = useStateValue();
 
-  const removeFromBasket = () => {
-    // remove the item from the basket
-    dispatch({
-      type: "REMOVE_FROM_BASKET",
-      id: id,
-    });
-  };
-  return (
-    <div className="checkoutProduct">
-      <img className="checkoutProduct_image" src={image} alt="" />
-      <div className="checkoutProduct_info">
-        <p className="checkoutProduct_title">{title}</p>
-        <p className="checkoutProduct_price">
-          <small>$</small>
-          <strong>{price}</strong>
-        </p>
-        <div className="checkoutProduct_rating">
-          {Array(rating)
-            .fill()
-            .map((_, i) => (
-              <p>⭐</p>
-            ))}
+    const removeFromBasket = () => {
+      // remove the item from the basket
+      dispatch({
+        type: "REMOVE_FROM_BASKET",
+        id: id,
+      });
+    };
+    return (
+      <div ref={ref} className="checkoutProduct">
+        <img className="checkoutProduct_image" src={image} alt="" />
+        <div className="checkoutProduct_info">
+          <p className="checkoutProduct_title">{title}</p>
+          <p className="checkoutProduct_price">
+            <small>$</small>
+            <strong>{price}</strong>
+          </p>
+          <div className="checkoutProduct_rating">
+            {Array(rating)
+              .fill()
+              .map((_, i) => (
+                <p>⭐</p>
+              ))}
+          </div>
+          <button onClick={removeFromBasket}>Remove from Basket</button>
         </div>
-        <button onClick={removeFromBasket}>Remove from Basket</button>
       </div>
-    </div>
-  );
-}
+    );
+  }
+);
 
 export default CheckoutProduct;
