eec7317a5a799bb1e8faf72405430fc45c50e9c7
mirfan46 <irfanmuh75@gmail.com>
2020-09-10 21:59:33 +0700

    add Say Hello

--- src/Header.js
+++ src/Header.js
@@ -32,7 +32,7 @@ function Header() {
       <div className="header_nav">
         <Link to={!user && "/login"}>
           <div onClick={handleAuthenticaton} className="header_option">
-            <span className="header_optionLineOne">Hello Guest</span>
+            <span className="header_optionLineOne">Hello {user?.email}</span>
             <span className="header_optionLineTwo">
               {user ? "Sign Out" : "Sign In"}
             </span>
